a30256dfc19f772822442f2ddeccebbc0412002a
liuweixiang <wliu0078@student.monash.edu>
2025-05-22 01:30:02 +1000

    feat: add IconHelper.kt for mapping weather descriptions to drawable icons

--- app/src/main/java/com/example/weatheroutfitassistant/util/weather/IconHelper.kt
+++ app/src/main/java/com/example/weatheroutfitassistant/util/weather/IconHelper.kt
@@ -1,2 +1,16 @@
-package com.example.weatheroutfitassistant.util.weather
+package com.example.myapplication.util.weather
 
+import com.example.myapplication.R
+
+fun getWeatherIcon(description: String): Int {
+    return when (description.lowercase()) {
+        "clear sky" -> R.drawable.ic_clear
+        "few clouds", "scattered clouds" -> R.drawable.ic_partly_cloudy
+        "broken clouds", "overcast clouds" -> R.drawable.ic_cloudy
+        "light rain", "moderate rain", "heavy rain", "shower rain" -> R.drawable.ic_rain
+        "thunderstorm" -> R.drawable.ic_thunderstorm
+        "snow" -> R.drawable.ic_snow
+        "mist", "fog", "haze" -> R.drawable.ic_mist
+        else -> R.drawable.ic_unknown
+    }
+}
